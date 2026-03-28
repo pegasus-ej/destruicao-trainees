175e992ef71036d295cece0bae2e617e65e8f4ea
João Vitor Capazório Domingos <pegasus.apucarana@unespar.edu.br>
2026-03-28 16:41:46 -0300

    chore: adiciona arquivos base

---
+++ script.js
@@ -0,0 +1,38 @@
+let bugsDestruidos = 0;
+
+function destruirBug() {
+
+    bugsDestruidos++;
+
+    document.getElementById("contador").textContent =
+        "Bugs destruídos: " + bugsDestruidos;
+
+    mostrarMensagem();
+
+}
+
+function mostrarMensagem() {
+
+    let mensagem = document.getElementById("mensagem");
+
+    if (bugsDestruidos === 5) {
+        mensagem.textContent =
+            "🔥 Você está pegando o jeito!";
+    }
+
+    else if (bugsDestruidos === 10) {
+        mensagem.textContent =
+            "⚡ Nível Júnior alcançado!";
+    }
+
+    else if (bugsDestruidos === 20) {
+        mensagem.textContent =
+            "🚀 Nível Pleno desbloqueado!";
+    }
+
+    else if (bugsDestruidos === 30) {
+        mensagem.textContent =
+            "🏆 Nível Sênior atingido!";
+    }
+
+}
